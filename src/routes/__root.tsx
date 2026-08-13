619bad436fc4e15c3fa2922d2217c8eb85c7da96
Vyshnavi <ramisettyvyshnavi95@gmail.com>
2026-08-13 10:11:16 +0530

    Add Vercel Analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,7 +8,7 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
-
+import { Analytics } from "@vercel/analytics/react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Toaster } from "@/components/ui/sonner";
@@ -140,10 +140,9 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
       <Toaster />
+      <Analytics />
     </QueryClientProvider>
-
   );
 }
